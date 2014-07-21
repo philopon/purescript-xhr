702efb636ff8996e5ff8c393dce7244cc97dc7e6
philopon <philopon.dependence@gmail.com>
2014-07-22 03:09:50 +0900

    update example.

--- examples/post.purs
+++ examples/post.purs
@@ -1,8 +1,10 @@
 module Main where
 
+import Control.Monad.Eff
 import Network.XHR
 import Debug.Trace
 
+main :: Eff (trace :: Trace, xhr :: XHR) Unit
 main = do
-    let cbs = defaultCallbacks { onLoadEnd = \x -> print (responseText x) }
-    post Multipart "/api/12" cbs {test: 24}
+    let cfg = defaultConfig { onLoadEnd = \x -> print (responseText x) }
+    post cfg "/api/12" {test: 24}
